8785d6619623f572914d4f07f6153d1ccb943464
XatMassacrE <xxat.massacre@gmail.com>
2019-09-09 14:44:33 +0800

    Update Header style

--- packages/extension-ui/src/pages/Header.tsx
+++ packages/extension-ui/src/pages/Header.tsx
@@ -1,32 +1,78 @@
 import React from "react";
 import { useState } from "react";
+import { Link } from 'react-router-dom';
+import Icon from '../components/Icon';
 import logo from "../assets/logo.jpg";
-import "./index.scss";
+import "./header.scss";
 
 function Header(props: any) {
-  const [showNodeList, setShowNodeList] = useState(false);
+  const [showNodeListArea, setShowNodeListArea] = useState(false);
+  const [showAccountArea, setShowAccountArea] = useState(false);
 
   return (
     <div className="header">
       <div className="container container-header">
-        <img className="logo" src={logo} alt="logo" />
+        <Link to="/">
+          <img className="logo" src={logo} alt="logo" />
+        </Link>
         <div className="right">
-          <div className="current-node">
+          <div className="current-node" onClick={() => {
+            setShowNodeListArea(!showNodeListArea)
+            setShowAccountArea(false)
+          }}>
             <span>HashQuark</span>
           </div>
-          <div className="setting" onClick={() =>
-            setShowNodeList(!showNodeList)
-          }><span>设置</span></div>
+          <div className="setting" onClick={() => {
+            setShowAccountArea(!showAccountArea)
+            setShowNodeListArea(false)
+          }}>
+            <Icon name="Menu" className="setting-icon" />
+          </div>
         </div>
           
       </div>
       {
-        showNodeList ? (
+        showNodeListArea && !showAccountArea ? (
         <div className="node-list-area">
-
+          <div className="node-list">
+            <div className="node-item">xixi</div>
+          </div>
+          <div className="divide-1">
+          </div>
+          <div className="auto-select">
+          <span>自动切换最优节点</span>
+          </div>
+          <div className="add-node">
+            <span>添加节点</span>
+          </div>
         </div>) : null
       }
-      
+      {
+        showAccountArea && !showNodeListArea ? (
+        <div className="account-area">
+          <div className="action">
+            <div>
+              <Link to="importAccount">
+                <Icon name="Putin" className="account-area-icon" />
+                <span>
+                  导入账户
+                </span>
+              </Link>
+            </div>
+            <div>
+              <Link to="newAccount">
+                <Icon name="Add" className="account-area-icon" />
+                <span>
+                  新建账户
+                </span>
+              </Link>
+            </div>
+          </div>
+          <div className="accounts">
+
+          </div>
+        </div>) : null
+      }
     </div>
   )
 }
