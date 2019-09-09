7aa6254407140d98493c8083366c0737a3891a82
Yongfeng LI <wliyongfeng@gmail.com>
2019-09-09 14:58:42 +0800

    Add test code for mnemonic

--- packages/extension-ui/src/pages/CreateAccount.tsx
+++ packages/extension-ui/src/pages/CreateAccount.tsx
@@ -1,6 +1,11 @@
 import React from "react";
+// @ts-ignore
+import Account from "@chainx/account";
 
 function CreateAccount() {
+  const mnemonic = Account.newMnemonic();
+  console.log(mnemonic);
+
   return (
     <div className="create-account">
       <span>CreateAccount</span>
