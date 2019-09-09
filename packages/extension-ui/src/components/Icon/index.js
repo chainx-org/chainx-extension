a072d3aee6864331c5069f32c6fe996cd1870293
XatMassacrE <xxat.massacre@gmail.com>
2019-09-09 14:42:57 +0800

    Add components

---
+++ packages/extension-ui/src/components/Icon/index.js
@@ -0,0 +1,10 @@
+import React, { PureComponent } from 'react';
+
+class Icon extends PureComponent {
+  render() {
+    const { className = {}, style = {}, name = '' } = this.props;
+    return <i className={`iconfont icon${name} ${className}`} style={style} />;
+  }
+}
+
+export default Icon;
