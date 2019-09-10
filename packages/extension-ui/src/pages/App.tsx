90f0369cc815a73de4f6e7a70ffecac827501cba
XatMassacrE <xxat.massacre@gmail.com>
2019-09-10 14:55:46 +0800

    Add RequestSign page router

--- packages/extension-ui/src/pages/App.tsx
+++ packages/extension-ui/src/pages/App.tsx
@@ -1,10 +1,12 @@
-import React from "react"; import { BrowserRouter as Router } from "react-router-dom";
-import { Redirect, Route, Switch } from "react-router";
-import Home from './Home';
-import Header from './Header';
-import CreateAccount from './CreateAccount';
-import ImportAccount from "./ImportAccount";
-import "./index.scss";
+import React from "react"
+import { BrowserRouter as Router } from "react-router-dom"
+import { Redirect, Route, Switch } from "react-router"
+import Home from './Home'
+import Header from './Header'
+import CreateAccount from './CreateAccount'
+import ImportAccount from './ImportAccount'
+import RequestSign from './RequestSign'
+import "./index.scss"
 
 export default function App() {
   return (
@@ -16,6 +18,7 @@ export default function App() {
             <Route exact path="/" component={Home} />
             <Route path="/createAccount" component={CreateAccount} />
             <Route path="/importAccount" component={ImportAccount} />
+            <Route path="/requestSign" component={RequestSign} />
             <Redirect to="/" />
           </Switch>
         </div>
