1151f1c52b6b024bc3756b5eb5b3182ca68a1a83
Yongfeng LI <wliyongfeng@gmail.com>
2019-09-10 16:19:39 +0800

    Refactor nodes

--- packages/extension/src/background/nodes.ts
+++ packages/extension/src/background/nodes.ts
@@ -3,19 +3,43 @@ import store from './store';
 import { ChainxNode } from "./types";
 
 export const NODE_PREFIX = 'node_';
+export const CURRENT_NODE_KEY = 'current_node_key';
 
 class Nodes {
   nodes: ChainxNode[];
+  currentNode: ChainxNode | null;
 
   constructor() {
     this.nodes = [];
+    this.currentNode = null;
+  }
+
+  _reset() {
+    this.nodes = [];
+    this.currentNode = null;
   }
 
   async loadAll(): Promise<ChainxNode[]> {
+    this._reset();
+
     await store.all((key, item) => {
       if (key.startsWith(NODE_PREFIX)) {
-        this.nodes.push({ name: key.slice(NODE_PREFIX.length), ...item });
+        this.nodes.push(item);
       }
+    })
+
+    if (this.nodes.length <= 0) {
+      return this.nodes;
+    }
+
+    await store.get(CURRENT_NODE_KEY, node => {
+      if (!node) {
+        this.currentNode = this.nodes[0] || null;
+        return;
+      }
+
+      const target = this.nodes.find(item => item.url === node.url);
+      this.currentNode = target || this.nodes[0];
     })
 
     return this.nodes;
