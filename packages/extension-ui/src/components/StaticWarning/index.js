60cdd0e0eb2c96c6eae051d1b5eaad25067327a1
XatMassacrE <xxat.massacre@gmail.com>
2019-09-11 15:57:20 +0800

    Add StaticWarning component to show some static warning page

---
+++ packages/extension-ui/src/components/StaticWarning/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import warningIcon from '../../assets/warning.png'
+import './index.scss'
+
+function StaticWarning(props) {
+  const { title='', desc='不要将您的助记词存储在电脑上，或者网上某处。任何拥有您助记词的人都能取用您的资金' } = props;
+
+  return (
+    <div className="static-warning">
+      <img className="warning-icon" src={warningIcon} alt="warning" />
+      <span className="warning-title">{title}</span>
+      <div className="warning-desc">{desc}</div>
+    </div>
+  )
+}
+
+export default StaticWarning
