4894896f4c0ae6d7d489b720e4d1f890bfde5325
Yongfeng LI <wliyongfeng@gmail.com>
2019-09-11 16:45:10 +0800

    Move constants to @chainx/extension-defaults

--- packages/extension-defaults/src/index.ts
+++ packages/extension-defaults/src/index.ts
@@ -2,10 +2,19 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-const PORT_CONTENT = 'chainx-content';
-const PORT_POPUP = 'chainx-popup';
-
-export {
-  PORT_CONTENT,
-  PORT_POPUP
-};
+export const PORT_CONTENT = 'chainx-content';
+export const PORT_POPUP = 'chainx-popup';
+export const CHAINX_ACCOUNT_CREATE = 'chainx.accounts.create';
+export const CHAINX_ACCOUNT_CREATE_FROM_PRIVATE = 'chainx.accounts.create.private'; // 从私钥创建账户
+export const CHAINX_ACCOUNT_EXPORT_PRIVATE = 'chainx.accounts.export.private'; // 导出账户私钥
+export const CHAINX_ACCOUNT_ALL = 'chainx.accounts.all';
+export const CHAINX_ACCOUNT_SELECT = 'chainx.accounts.select'; // 设置目标账户
+export const CHAINX_ACCOUNT_CURRENT = 'chainx.accounts.current'; // 获取当前账户
+export const CHAINX_ACCOUNT_REMOVE = 'chainx.accounts.remove';
+export const CHAINX_ACCOUNT_SIGN_MESSAGE = 'chainx.accounts.sign.message';
+export const CHAINX_TRANSACTION_SIGN = 'chainx.transaction.sign';
+export const CHAINX_NODE_CREATE = 'chainx.nodes.create';
+export const CHAINX_NODE_ALL = 'chainx.nodes.all';
+export const CHAINX_NODE_SELECT = 'chainx.nodes.select'; // 设置节点
+export const CHAINX_NODE_CURRENT = 'chainx.nodes.current'; // 获取当前节点
+export const CHAINX_NODE_REMOVE = 'chainx.nodes.remove';
