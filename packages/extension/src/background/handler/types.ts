0dd638f7ec62b61a1970bd070355f2815d6e64e1
Yongfeng LI <wliyongfeng@gmail.com>
2019-09-12 12:04:32 +0800

    Add func to sign transaction

--- packages/extension/src/background/handler/types.ts
+++ packages/extension/src/background/handler/types.ts
@@ -4,3 +4,11 @@ export interface MessageRequest {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   request: any;
 }
+
+export interface ChainxSignRequest {
+  address: string, // 用于确定是那个账户的请求
+  password: string, // 用于签名
+  module: 'xAssets' | 'xStaking',
+  method: string,
+  args: Array<any>
+}
