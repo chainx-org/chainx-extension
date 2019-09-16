bc0f137cc9175cb70ec4c115644113aa655fd000
XatMassacrE <xxat.massacre@gmail.com>
2019-09-16 10:35:32 +0800

    Add account info and error display

--- packages/extension-ui/src/pages/RequestSign/index.tsx
+++ packages/extension-ui/src/pages/RequestSign/index.tsx
@@ -1,19 +1,46 @@
-import React, { useState } from 'react'
-import { signMessage } from '../../messaging'
+import React, { useState, useEffect } from 'react'
+import { signMessage, getCurrentChainxAccount } from '../../messaging'
+import { useRedux } from '../../shared'
+import ErrorMessage from '../../components/ErrorMessage';
 import './requestSign.scss'
 
 function RequestSign(props: any) {
   const [sig, setSig] = useState('')
   const [pass, setPass] = useState('')
+  const [errMsg, setErrMsg] = useState('')
+  const [{currentAccount}, setCurrentAccount] = useRedux('currentAccount', { address: '', name: ''})
+
+  useEffect(() => {
+    getCurrentAccount()
+  }, [])
+
+  const getCurrentAccount = async () => {
+    const result =  await getCurrentChainxAccount()
+    setCurrentAccount({ currentAccount: result })
+  }
+
+  const check = () => {
+    if (!pass) {
+      setErrMsg('password is required')
+      return false
+    }
+    return true
+  }
 
   const sign = async () => {
+    if (!currentAccount.address) {
+      setErrMsg(`Error: address is not exist`)
+    }
+    if (!check()) {
+      return
+    }
     try {
-      const result = await signMessage('5QNKsFw5FYBUst68sHuW6CiMKY3FkJ6sLhof4Qw2xhRuRmxn', 'message', pass)
+      const result = await signMessage(currentAccount.address, 'message', pass)
       console.log('sign message ', result)
+      setErrMsg('')
       setSig(result)
     } catch (e) {
-      setSig(`Error: ${e.message}`)
-      console.log('page error:', e);
+      setErrMsg(`Error: ${e.message}`)
     }
   }
   return (
@@ -49,12 +76,16 @@ function RequestSign(props: any) {
           placeholder='密码'
         />
         <div className="button-area margin-top-40">
-          <button className="button button-white-half" onClick={() => props.history.push('/')}>取消</button>
+          <button className="button button-white-half" onClick={() => {
+            props.history.push('/')
+            window.close()
+          }}>取消</button>
           <button className="button button-yellow-half" onClick={() => {
             sign()
           }}>签名</button>
         </div>
-        <span>sign result: {sig}</span>
+        <span className="result">sign result: {sig}</span>
+        <ErrorMessage msg={errMsg} />
       </div>
     </div>
   )
