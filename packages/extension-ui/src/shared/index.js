d84b0bfd24d44990890b9689521fff352647ddfd
XatMassacrE <xxat.massacre@gmail.com>
2019-09-17 15:38:59 +0800

    Add fetch.js to detect node delay

--- packages/extension-ui/src/shared/index.js
+++ packages/extension-ui/src/shared/index.js
@@ -2,3 +2,4 @@ export { default as useRedux } from "./useRedux";
 export { default as createStore } from "./createStore";
 export { default as shallowEqual } from "./shallowEqual";
 export { default as useOutsideClick } from './useClickOutside';
+export { default as fetchFromWs } from './fetch';
