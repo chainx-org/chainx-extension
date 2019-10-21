fec24a355276e7aeab556a2affe7e013fbef46a9
Yongfeng LI <wliyongfeng@gmail.com>
2019-10-21 15:52:17 +0800

    Change Icon to function component

--- packages/extension-ui/src/components/Icon/index.tsx
+++ packages/extension-ui/src/components/Icon/index.tsx
@@ -1,11 +1,8 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 
-class Icon extends PureComponent {
-  render() {
-    // @ts-ignore
-    const { className = {}, style = {}, name = '' } = this.props;
-    return <i className={`iconfont icon${name} ${className}`} style={style} />;
-  }
+function Icon(props) {
+  const { className = {}, style = {}, name = '' } = props;
+  return <i className={`iconfont icon${name} ${className}`} style={style} />;
 }
 
 export default Icon;
