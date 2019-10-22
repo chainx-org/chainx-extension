7390c13a4599128d37340e8a742c24de2a491aeb
Yongfeng LI <wliyongfeng@gmail.com>
2019-10-22 17:06:34 +0800

    Set transfer info to english

--- packages/extension-ui/src/pages/RequestSign/Transfer.tsx
+++ packages/extension-ui/src/pages/RequestSign/Transfer.tsx
@@ -8,21 +8,21 @@ export default function(props) {
   return (
     <div className="detail">
       <div className="detail-item">
-        <span>操作</span>
+        <span>Operation</span>
         <span>{query.method}</span>
       </div>
       <div className="detail-item">
-        <span>转账数量</span>
+        <span>Amount</span>
         <span>
           {toPrecision(query.args[2], pcxPrecision)} {query.args[1]}
         </span>
       </div>
       <div className="detail-item">
-        <span>接收人地址</span>
+        <span>Dest</span>
         <span>{query.args[0]}</span>
       </div>
       <div className="detail-item">
-        <span>备注</span>
+        <span>Memo</span>
         <span>{query.args[3]}</span>
       </div>
     </div>
