3fcd5fb90b79ef443a3a07bec7fcf0ccad588c16
Yongfeng LI <wliyongfeng@gmail.com>
2019-12-18 11:44:14 +0800

    Remove useless function

--- packages/extension-ui/src/messaging/index.ts
+++ packages/extension-ui/src/messaging/index.ts
@@ -11,7 +11,6 @@ import {
   removeChainxNode,
   setChainxCurrentAccount,
   setChainxNode,
-  signMessage,
   setNetwork,
   getSettings
 } from './popup';
@@ -28,7 +27,6 @@ export {
   getCurrentChainxNode,
   removeChainxNode,
   createChainxNode,
-  signMessage,
   getAllAccounts,
   createAccount,
   createAccountFromPrivateKey,
