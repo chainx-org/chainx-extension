70a3ed4492b5c4eb12b51a8b4ae3c334f83205df
Yongfeng LI <wliyongfeng@gmail.com>
2020-05-25 18:06:12 +0800

    Not action when set to current node

--- packages/extension-ui/src/pages/Header/index.tsx
+++ packages/extension-ui/src/pages/Header/index.tsx
@@ -17,11 +17,9 @@ import AccountPanelSwitch from '@chainx/extension-ui/pages/Header/AccountPanelSw
 import NodesPanelSwitch from '@chainx/extension-ui/pages/Header/NodesPanelSwitch';
 import SignHeader from '@chainx/extension-ui/pages/Header/SignHeader';
 import Logo from './Logo';
-import {
-  isTestNetSelector,
-  setIsTestNet as setStoreIsTestNet
-} from '../../store/reducers/networkSlice';
+import { isTestNetSelector, setIsTestNet as setStoreIsTestNet } from '../../store/reducers/networkSlice';
 import initNodes, { updateDelay } from '@chainx/extension-ui/shared/nodeUtils';
+import { currentMainNetNodeSelector, currentTestNetNodeSelector } from "@chainx/extension-ui/store/reducers/nodeSlice";
 
 function Header(props: any) {
   const refNodeList = useRef<HTMLInputElement>(null);
@@ -29,6 +27,9 @@ function Header(props: any) {
   const dispatch = useDispatch();
   const showAccountMenu = useSelector(showAccountMenuSelector);
   const showNodeMenu = useSelector(showNodeMenuSelector);
+  const currentNode = useSelector(
+    isTestNet ? currentTestNetNodeSelector : currentMainNetNodeSelector
+  );
 
   useEffect(() => {
     const intervalId = setInterval(() => {
@@ -45,8 +46,11 @@ function Header(props: any) {
   });
 
   async function setNode(url: string) {
-    dispatch(setInitLoading(true));
+    if (url === currentNode.url) {
+      return
+    }
 
+    dispatch(setInitLoading(true));
     // TODO: 目前background和ui保存了两个ChainX实例，后边考虑下如何优化
     await Promise.race([setChainxNode(url, isTestNet), sleep(2000)]);
     initNodes()
